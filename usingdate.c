beeeeb6af477a5712501e9e3733efc146c4568a2
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-05 21:51:36 +0530

    Add files via upload

---
+++ usingdate.c
@@ -0,0 +1,53 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int main(int argc, char const *argv[])
+{
+	
+
+	if (argc==1){
+		time_t t ;
+	    char mytime[100];
+	    time( &t );
+	     
+	    struct tm *tmp = localtime( &t );
+	     
+	    // using strftime to display time
+	    strftime(mytime, sizeof(mytime), "%A %d %B %Y %I:%M:%S %p", tmp);
+	     
+	    printf("%s\n", mytime);
+	}
+
+	else if (argc==2){
+		if (strcmp(argv[1],"+%B")==0){
+			time_t t ;
+		    char mytime[100];
+		    time( &t );
+		     
+		    struct tm *tmp = localtime( &t );
+		     
+		    // using strftime to display time
+		    strftime(mytime, sizeof(mytime), "%B", tmp);
+		     
+		    printf("%s\n", mytime);
+		}
+
+		else if (strcmp(argv[1],"+%Y")==0){
+			time_t t ;
+		    char mytime[100];
+		    time( &t );
+		     
+		    struct tm *tmp = localtime( &t );
+		     
+		    // using strftime to display time
+		    strftime(mytime, sizeof(mytime), "%Y", tmp);
+		     
+		    printf("%s\n", mytime);
+		}
+	}
+
+	
+	return 0;
+}
