2e13f32afaf189244fd86b46e1ff9c30fe3a4e60
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-05 19:47:23 +0530

    Update usingcd.c

--- usingcd.c
+++ usingcd.c
@@ -4,11 +4,33 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+//#include <dir.h>
 
 
 int main(int argc, char * argv[]){
-    char path[1000];
-    chdir("..");
-    printf("%s\n",getcwd(path,1000));
+    
+
+
+	if (argc==1){
+		printf("%s","Need two arguments\n");
+		return 0;
+	}
+
+	else if (argc>2){
+		printf("%s","Need only two arguments!");
+		return 0;
+	}
+
+	else{
+		char path[1000];
+		char abspath[1000]="/home/parthiv9221/Desktop/Assignment2/";
+		strcat(abspath, argv[1]);
+	    chdir(abspath);
+		printf("%s\n",getcwd(path,1000));
+	}
+
+
+
     return 0;
 }
+
