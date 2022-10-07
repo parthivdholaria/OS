c1624ac46caacfbe9a0106f2d7b65ca1f45e6f0f
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-08 00:55:13 +0530

    Update usingrm.c

--- usingrm.c
+++ usingrm.c
@@ -3,33 +3,40 @@
 #include <ftw.h>
 #include <string.h>
 #include <unistd.h>
-
-
-static int removeFiles(char * path, struct stat * sbuf, int type, struct FTW * ftwb){
-	if (remove(path)<0){
-		printf("%s","Could not delet the file");
-		return -1;
-	}
-
-	return 0;
-}
+#include <dirent.h>
 
 
 
 
 int main(int argc, char ** argv){
 
-	if (argc != 2)
+	if (argc < 2)
     {
-        printf("%s","Need to two arguments!");
+        printf("%s","Need two arguments!");
         return 0;
     }
 
     else if (argc==2){
-    	if (nftw(argv[1],removeFiles, 10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS)<0){
-    		printf("%s","Error in deleting your file");
-    		return 0;
-    	}
+
+        if (strcmp(argv[1],"--version")==0){
+            printf("%s","This is my custom rm with version 1.0.0\n");
+            return 0;
+        }
+        
+        if (remove(argv[1])==0){
+            printf("%s","FILE deleted successfully\n");
+            return 0;
+        }
+
+
+    }
+
+    else if (argc==3){
+        if (strcmp(argv[1],"-v")==0){
+            remove(argv[2]);
+            printf("Removed using custom shell:%s\n",argv[2]);
+        }
+        
     }
 
     return 0;
