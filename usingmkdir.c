d5f5a473087251c1fc4c314bd5e6ab2090cdf1ef
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-08 00:54:37 +0530

    Update usingmkdir.c

--- usingmkdir.c
+++ usingmkdir.c
@@ -3,12 +3,13 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <string.h>
 
 
 int main(int argc,char * argv[]){
 
 	if (argc<=1){
-		printf("Few arguments given give atleast 2 arguments!");
+		printf("Few arguments given give atleast 2 arguments!\n");
 		return 0;
 	}
 
@@ -18,8 +19,32 @@ int main(int argc,char * argv[]){
 		int ret=mkdir(directory,0777);
 
 		if (ret==-1){
-			printf("Failed to create to directory!");
+			printf("Failed to create to directory!\n");
 			return 0;
+		}
+	}
+
+	else if (argc==3){
+		if (strcmp(argv[1],"-p")==0){
+			char cmmd[100]="mkdir -p ";
+			strcat(cmmd,argv[2]);
+			system(cmmd);
+			int ret=mkdir(argv[2],0777);
+		}
+		else if (strcmp(argv[1],"-v")==0){
+			char * dir1=argv[2];
+
+			int ret1=mkdir(dir1,0777);
+
+
+			if (ret1==-1){
+				printf("Failed to create to directory%s!\n",dir1);
+				return 0;
+			}
+			else{
+				printf("Custom mkdir: created a directory: %s\n",dir1);
+			}
+
 		}
 	}
 
