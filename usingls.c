8d48c3cd6c9057f4598227c510bb87a1dc1a0e0d
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-08 00:53:35 +0530

    Update usingls.c

--- usingls.c
+++ usingls.c
@@ -1,43 +1,8 @@
 #include <stdio.h>
 #include<unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
-
-void ls(char * dir, int a, int l){
-	DIR * currdir = opendir(dir);
-	if (!currdir){
-		printf("%s\n","Invalid command! Directory does not exist or Unable to read directory!");
-		return;
-	}
-
-	struct dirent* entity;
-	entity=readdir(currdir);
-
-
-	while (entity!=NULL){
-
-		if (!a && entity->d_name[0]=='.'){
-			continue;
-		}
-
-		printf("%s  ",entity->d_name);
-		
-		if (l){
-			printf("\n");
-		}
-
-		entity=readdir(currdir);
-
-		
-	}
-
-	if (!l){
-		printf("\n");
-	}
-
-	closedir(currdir);
-
-}
 
 
 int main(int argc,char *argv[]){
@@ -66,35 +31,16 @@ int main(int argc,char *argv[]){
 	}
 
 	else if (argc==2){
-		if (argv[1][0]=='-'){
-			int a=0;
-			int l=0;
 
-			char * itr = (char *)(argv[1]+1);
-
-			while (*itr){
-				if (*itr=='a'){
-					a=1;
-				}
-
-				else if (*itr=='l'){
-					l=1;
-				}
-
-				else{
-					printf("%s\n","Invalid input for the ls command!");
-					return 0;
-				}
-
-				itr++;
-
-			}
-
-			ls(".",a,l);
-
-			}
+		if (strcmp(argv[1],"-l")==0){
+			system("ls -l");
 		}
 
+		else if (strcmp(argv[1],"-a")==0){
+			system("ls -a");
+		}
 
+	}
+		
 	return 0;
 }
