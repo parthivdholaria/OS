4652cd7c242ea132da3bd259368a66ede6139d86
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-09 15:49:48 +0530

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -6,90 +6,47 @@
 #include <sys/wait.h>
 
 
-char ** parse(char * input){
-
-	int noofbuffer = 100;
-	char ** tokens = malloc(noofbuffer*sizeof(char *));
-	int tokens_counter=0;
+#define splitters " \t\r\n\a"
 
 
 
-	int counttokenbuffer = 1024;
-
-	char * current_token=(char *)malloc(counttokenbuffer*sizeof(char));
-
-	int current_token_counter = 0;
-
-    for (int i = 0; i < strlen(input); i++) {
-
-    	char character = input[i];
-
-        if (character == ' ') {
-
-            if (tokens_counter + 1 > noofbuffer) {
-                noofbuffer += 100;
-                tokens = realloc(tokens, noofbuffer * sizeof(char*));
-
-            }
-
-            
-
-            if (current_token_counter + 1 >= counttokenbuffer) {
-
-                counttokenbuffer += 1024;
-
-                current_token = realloc(current_token, counttokenbuffer);
-
-            }
-
-            tokens[tokens_counter] = current_token;
-            tokens_counter++;
-
-            current_token = malloc(counttokenbuffer * sizeof(char*));
-            current_token_counter = 0;
-
-        } else if (character == '\n') {
-
-            if (tokens_counter + 2 > noofbuffer) {
-                noofbuffer += 100;
-                tokens = realloc(tokens, noofbuffer * sizeof(char*));
-
-            }
-
-            if (current_token_counter + 1 > counttokenbuffer) {
-                counttokenbuffer += 1024;
-                current_token = realloc(current_token, counttokenbuffer);
-
-            }
-
-            
-
-            tokens[tokens_counter] = current_token;
-            tokens_counter++;
+char ** parse(char * cmmd){
 
 
+	int buffsize=64;
+	int pos=0;
 
-            tokens[tokens_counter] = NULL;
-            break;
+	char ** tokens= (char **) malloc (buffsize*sizeof(char *));
+
+	char * token;
+
+	if (tokens==NULL){
+		printf("Could not allocate memory\n");
+		exit(1);
+	}
+
+	token = strtok(cmmd,splitters);
+
+	while (token!=NULL){
+		tokens[pos]=token;
+		pos++;
 
 
-        } else {
+		if (pos>=buffsize){
+			buffsize+=64;
+			tokens=realloc(tokens,buffsize*sizeof(char  *));
+			if (tokens==NULL){
+				printf("Could not allocate memory while reallocating \n");
+				exit(1);
+			}
+		}
 
-            if (current_token_counter + 1 > counttokenbuffer) {
+		token=strtok(NULL,splitters);
 
-                counttokenbuffer += 1024;
-                current_token = realloc(current_token, counttokenbuffer);
+	}
 
-            }
-
-            current_token[current_token_counter] = character;
-            current_token_counter++;
-
-        }
-
-    }
-
-    return tokens;	
+	tokens[pos]=NULL;
+	return tokens;
 
 }
 
@@ -155,30 +112,22 @@ int main(int argc, char * argv[]){
 
 
 
-			if(strcmp(parsed[0],"pwd")==0){
-				char buffpath[100];
-				getcwd(buffpath,sizeof(buffpath));
-				printf("%s\n",buffpath);
-				// strcat(buffpath,"/pwd");
-				// execl(buffpath,buffpath,NULL,NULL,NULL);	
+			if(strcmp(parsed[0],"pwd")==0){			
+				strcat(home,"/pwd");
+				execl(home,home,parsed[1],NULL,NULL);
+
 
 			}
 
 
 
 			else if (strcmp(parsed[0],"ls")==0){
-
+		
 				strcat(home,"/ls");
+				execl(home,home,parsed[1],NULL,NULL);
 
-				if (strcmp(parsed[1],"-l")==0 || strcmp(parsed[1],"-a")==0){
-					execl(home,home,parsed[1],NULL,NULL);
-				}
-				else{
-					execl(home,home,NULL,NULL,NULL);	
-				}
 
 			}
-
 
 
 			else if (strcmp(parsed[0],"cat")==0){
@@ -194,70 +143,39 @@ int main(int argc, char * argv[]){
 
 
 				strcat(home,"/echo");
-
-				if(strcmp(parsed[1],"-e")==0){
-					execl(home,home,parsed[1],parsed[2],NULL);
-				}
-
-				else if (strcmp(parsed[1],"-n")==0){
-					execl(home,home,parsed[1],parsed[2],NULL);
-				}
-				else{
-					execl(home,home,parsed[1],NULL,NULL);
-				}
-
+				execl(home,home,parsed[1],parsed[2],NULL);
 				
 
 			}
 
 			else if (strcmp(parsed[0],"cd")==0){
 
-
 				if (chdir(parsed[1])!=0){
 					printf("%s","error occured!");
 				}
-				// execl("/home/parthiv9221/Desktop/Assignment2/cd","/home/parthiv9221/Desktop/Assignment2/cd",parsed[1],NULL,NULL);
 			}
 
 			else if (strcmp(parsed[0],"date")==0){
 
 
 				strcat(home,"/date");
+				execl(home,home,parsed[1],NULL,NULL);
 
-
-				if(strcmp(parsed[1],"+%B")==0 || strcmp(parsed[1],"+%Y")==0){
-
-					execl(home,home,parsed[1],NULL,NULL);
-				}
-				else{
-					execl(home,home,NULL,NULL,NULL);	
-				}
 				
 			}
 
 			else if (strcmp(parsed[0],"mkdir")==0){
 
 				strcat(home,"/mkdir");
+				execl(home,home,parsed[1],parsed[2],NULL);	
 
-				if (strcmp(parsed[1],"-p")==0 || strcmp(parsed[1],"-v")==0){
-					execl(home,home,parsed[1],parsed[2],NULL);	
-				}
-				else{
-					execl(home,home,parsed[1],NULL,NULL);
-				}
 			}
 
 			else if (strcmp(parsed[0],"rm")==0){
 
 
 				strcat(home,"/rm");
-
-				if (strcmp(parsed[1],"-v")==0){
-					execl(home,home,parsed[1],parsed[2],NULL);
-				}
-				else{
-					execl(home,home,parsed[1],NULL,NULL);	
-				}
+				execl(home,home,parsed[1],parsed[2],NULL);	
 
 			}
 
@@ -267,9 +185,6 @@ int main(int argc, char * argv[]){
 				execl(home,home,NULL,NULL,NULL);	
 			}
 
-		
-	
-
 		}
 
 		if (strcmp(parsed[0],"exit")==0){
@@ -278,15 +193,10 @@ int main(int argc, char * argv[]){
 
 		}	
 
-
 		free(input);
 		free(parsed);
 
-
-
 	}
-
-
 
 	return 0;
 
