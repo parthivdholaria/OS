6cc860071ba1a95acb634db47020391e851578fc
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-09 16:12:58 +0530

    Update usingcat.c

--- usingcat.c
+++ usingcat.c
@@ -4,19 +4,41 @@
 
 int main(int argc,char * argv[]){
 
-	 FILE * fptr=fopen(argv[1],"r");
 
-	 if (argc!=2 || !fptr){
-	 	printf("%s","2 arguments needed! or file could not open\n");
-	 	return 0;
-	 }
 
-	 char c;
-	 while ((c=fgetc(fptr))!=EOF){
-	 	printf("%c",c);
-	 }
 
-	fclose(fptr);
+	if (argc==2){
+		
+		FILE * fptr=fopen(argv[1],"r");
+
+		if (!fptr){
+			printf("%s","2 arguments needed! or file could not open\n");
+			return 0;
+		}
+		char c;
+		while ((c=fgetc(fptr))!=EOF){
+			printf("%c",c);
+		}
+
+		fclose(fptr);
+	}
+
+	else if (argc==3){
+		if (strcmp(argv[1],"-n")==0){
+			char cmmd[100]="cat -n ";
+			strcat(cmmd,argv[2]);
+			system(cmmd);
+		}
+		else if (strcmp(argv[1],">")==0){
+			FILE * fptr=fopen(argv[2],"w");
+			if (!fptr){
+				printf("Could not create the file\n");
+				return 0;
+			}
+			printf("File created successfully\n");
+		}
+	}
+	 
 
 
 	return 0;
