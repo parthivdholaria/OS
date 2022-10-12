7d27c2a101fb375e78b4ad13b323839bee435f76
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-12 22:44:10 +0530

    Update usingrm.c

--- usingrm.c
+++ usingrm.c
@@ -23,10 +23,21 @@ int main(int argc, char ** argv){
             return 0;
         }
         
+
+        if (strcmp(argv[1],"--help")==0){
+            printf("%s"," 1) --version: to see the version of the rm function\n");
+            printf("%s"," 2) -v: to see what is being done\n");
+
+            return 0;
+        }
+
+
         if (remove(argv[1])==0){
             printf("%s","FILE deleted successfully\n");
             return 0;
         }
+
+        
 
 
     }
@@ -35,6 +46,10 @@ int main(int argc, char ** argv){
         if (strcmp(argv[1],"-v")==0){
             remove(argv[2]);
             printf("Removed using custom shell:%s\n",argv[2]);
+        }
+
+        if (strcmp(argv[1],"-f")==0){
+            unlink(argv[2]);
         }
         
     }
