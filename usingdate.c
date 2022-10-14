41eb0d5c6cd530e7ab2364d1fc8ccf2bf4aaedcb
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-10-14 16:27:51 +0530

    Update usingdate.c

--- usingdate.c
+++ usingdate.c
@@ -45,6 +45,31 @@ int main(int argc, char *argv[])
 		     
 		    printf("%s\n", mytime);
 		}
+		else if (strcmp(argv[1],"-u")==0){
+			time_t t ;
+		    char mytime[100];
+		    time( &t );
+		     
+		    struct tm *tmp = gmtime( &t );
+		     
+		    // using strftime to display time
+	    	strftime(mytime, sizeof(mytime), "%A %d %B %Y %I:%M:%S %p UTC", tmp);
+		     
+		    printf("%s\n", mytime);
+		}
+
+		else if (strcmp(argv[1],"-R")==0){
+			time_t t ;
+		    char mytime[100];
+		    time( &t );
+		     
+		    struct tm *tmp = localtime( &t );
+		     
+		    // using strftime to display time
+	    	strftime(mytime, sizeof(mytime), "%a, %d %^b %Y %H:%M:%S +0530", tmp);
+		     
+		    printf("%s\n", mytime);
+		}
 	}
 
 	
