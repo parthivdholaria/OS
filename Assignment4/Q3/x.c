64ce7178ef6e7b88bacca9a891093d5d979bdfbc
parthivdholaria <97287981+parthivdholaria@users.noreply.github.com>
2022-12-21 00:02:48 +0530

    Update x.c

--- Assignment4/Q3/x.c
+++ Assignment4/Q3/x.c
@@ -33,13 +33,11 @@ static int __init read_struct(void){
 	struct_pid = find_get_pid(myint);
 	task = pid_task(struct_pid,PIDTYPE_PID);
 
-	pgid=pid_vnr(task_pgrp(task));
-
 
 	printk(KERN_INFO "process id= %d\n",task->pid);
 	printk(KERN_INFO "process user_id= %d\n",task->cred->uid.val);
 	printk(KERN_INFO "process command path: %s\n",task->comm);
-	printk(KERN_INFO "process group id is= %d\n",pgid);
+	printk(KERN_INFO "process group id is= %d\n",task->tgid);
 	return 0;
 }
 
